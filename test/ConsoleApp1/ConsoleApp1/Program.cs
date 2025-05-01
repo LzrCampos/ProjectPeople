2f3e31d4d8f582e8f21e96ffe03260e370875ce7
lzrCampos <lazarocampos98@outlook.com>
2025-05-01 16:01:58 -0300

    feat: Adicionando pastas para o projeto

---
+++ test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        var mage = new Mage("Patolino");
+        var warrrior = new Warrior("Pertalonga");
+
+        mage.Atack();
+        warrrior.Atack();
+    }
+}
+
+public abstract class Skill
+{
+    public string name { get; set; }
+    public abstract void Atack();
+
+    public void FormatDamage(int damage, string name)
+    {
+        Console.WriteLine($"O ataque de {name} causou {damage} pontos de dano."); 
+    }
+}
+
+public class Mage : Skill
+{
+    public int Magika { get; private set; }
+    public Mage(string name)
+    {
+        this.name = name;
+        this.Magika = 10;
+    }
+
+    public override void Atack()
+    {
+        if (this.Magika == 0)
+        {
+            Console.WriteLine("Voce não possui mana o suficiente.");
+        }
+        else
+        {
+            this.Magika--;
+            int damage = new Random().Next(1, 13);
+            FormatDamage(damage, this.name);
+        }
+    }
+}
+
+public class Warrior : Skill
+{
+    public Warrior(string name)
+    {
+        this.name = name;
+    }
+
+    public override void Atack()
+    {
+        int damage = new Random().Next(1, 7);
+        FormatDamage(damage, this.name);
+    }
+}
